c4588e4b5ed749e36a28f053d69051b0982ff236
pnb3431 <pete.bailen@gmail.com>
2016-09-30 11:15:17 -0500

    made templates and contact

--- client/main.js
+++ client/main.js
@@ -1,4 +1,20 @@
 
 $(document).ready(function(){
       $('.parallax').parallax();
-    });
+});
+
+
+
+Router.route('/', function () {
+  this.layout('ApplicationLayout');
+  // render the Post template into the "main" region
+  // {{> yield}}
+  this.render('Home');
+});
+
+Router.route('/contact', function () {
+this.layout('ApplicationLayout');
+  // render the Post template into the "main" region
+  // {{> yield}}
+  this.render('Contact');
+});
