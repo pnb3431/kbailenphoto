db7fae3158846c8bd0eccf733920ee68031a5b7a
pnb3431 <pete.bailen@gmail.com>
2016-09-30 14:43:10 -0500

    added routes

--- client/main.js
+++ client/main.js
@@ -2,3 +2,10 @@
 $(document).ready(function(){
       $('.parallax').parallax();
     });
+
+Router.route('/', function () {
+  this.layout('ApplicationLayout');
+  // render the Post template into the "main" region
+  // {{> yield}}
+  this.render('Home');
+});
