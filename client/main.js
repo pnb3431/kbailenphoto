ca5122b4888246514f4e54448972aaf8df440cd6
pnb3431 <pete.bailen@gmail.com>
2016-10-01 23:25:00 -0500

    everything working

--- client/main.js
+++ client/main.js
@@ -1,9 +1,85 @@
 
-$(document).ready(function(){
-      $('.parallax').parallax();
-});
+
+
 
 
 Router.route('/', function () {
   this.render('Home');
+  this.layout('ApplicationLayout');
+  
+
 });
+
+Router.route('/pricing', function () {
+  this.render('pricing');
+  this.layout('ApplicationLayout');
+  
+
+});
+
+Router.route('/contact', function () {
+  this.render('contact');
+  this.layout('ApplicationLayout');
+  
+
+});
+Router.route('/families', function () {
+  this.render('families');
+  this.layout('ApplicationLayout');
+  
+
+});
+Router.route('/children', function () {
+  this.render('families');
+  this.layout('ApplicationLayout');
+  
+
+});
+Router.route('/babies', function () {
+  this.render('families');
+  this.layout('ApplicationLayout');
+  
+
+});
+
+Router.route('/maternity', function () {
+  this.render('families');
+  this.layout('ApplicationLayout');
+  
+
+});
+Router.route('/seniors', function () {
+  this.render('families');
+  this.layout('ApplicationLayout');
+});
+Router.route('/couples', function () {
+  this.render('families');
+  this.layout('ApplicationLayout');
+});
+Router.route('referrals', function () {
+  this.render('referrals');
+  this.layout('ApplicationLayout');
+  
+
+});
+
+Template.ApplicationLayout.onRendered(function(){
+	$('.parallax').parallax();
+	$('.dropdown-button').dropdown();
+	$('.carousel').carousel();
+	$('.materialboxed').materialbox();
+  	$('#textarea1').trigger('autoresize')
+});
+
+Template.Home.onRendered(function(){
+	$('.parallax').parallax();
+});
+
+Template.families.onRendered(function(){
+	$('.carousel').carousel();
+	$('.materialboxed').materialbox();
+});
+Template.contact.onRendered(function(){
+	$('#textarea1').trigger('autoresize')
+});
+
