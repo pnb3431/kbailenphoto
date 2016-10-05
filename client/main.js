3a3c384cbf3da11eee6c454773e79d1d1113f710
pnb3431 <pete.bailen@gmail.com>
2016-10-05 14:29:55 -0500

    not working uing the meteor chef

--- client/main.js
+++ client/main.js
@@ -83,8 +83,18 @@ Router.route('/couples', function () {
   this.render('title', {to: 'title'});
   this.layout('ApplicationLayout');
 });
-Router.route('referrals', function () {
+Router.route('/referrals', function () {
   this.render('referrals');
+  this.render('navbar', {to: 'navbar'});
+  this.render('footer', {to: 'footer'});
+  this.render('title', {to: 'title'});
+  this.layout('ApplicationLayout');
+  
+
+});
+
+Router.route('/admin', function () {
+  this.render('upload');
   this.render('navbar', {to: 'navbar'});
   this.render('footer', {to: 'footer'});
   this.render('title', {to: 'title'});
@@ -98,7 +108,7 @@ Template.ApplicationLayout.onRendered(function(){
 	$('.dropdown-button').dropdown();
 	$('.carousel').carousel();
 	$('.materialboxed').materialbox();
-  	$('#textarea1').trigger('autoresize')
+  $('#textarea1').trigger('autoresize')
 });
 
 Template.Home.onRendered(function(){
@@ -112,4 +122,113 @@ Template.families.onRendered(function(){
 Template.contact.onRendered(function(){
 	$('#textarea1').trigger('autoresize')
 });
+
+Template.upload.events({
+  'change input[type="file"]' ( event, template ) {
+    let _getFileFromInput = ( event ) => event.target.files[0];
+
+  let _setPlaceholderText = ( string = "Click or Drag a File Here to Upload" ) => {
+    template.find( ".alert span" ).innerText = string;
+  };
+
+  let _addUrlToDatabase = ( url ) => {
+    Meteor.call( "storeUrlInDatabase", url, ( error ) => {
+      if ( error ) {
+        Bert.alert( error.reason, "warning" );
+        _setPlaceholderText();
+      } else {
+        Bert.alert( "File uploaded to Amazon S3!", "success" );
+        _setPlaceholderText();
+      }
+    });
+  };
+
+  let _uploadFileToAmazon = ( file ) => {
+    const uploader = new Slingshot.Upload( "uploadToAmazonS3" );
+
+    uploader.send( file, ( error, url ) => {
+      if ( error ) {
+        Bert.alert( error.message, "warning" );
+        _setPlaceholderText();
+      } else {
+        _addUrlToDatabase( url );
+      }
+    });
+  };
+
+    
+  }
+});
+
+Template.upload.helpers({
+  isImage( url ) {
+    const formats = [ 'jpg', 'jpeg', 'png', 'gif' ];
+    return _.find( formats, ( format ) => url.indexOf( format ) > -1 );
+  }
+});
+
+Template.upload.onCreated( () => Template.instance().subscribe( 'upload' ) );
+
+Template.upload.helpers({
+  files() {
+    var files = Files.find( {}, { sort: { "added": -1 } } );
+    if ( files ) {
+      return files;
+    }
+  }
+});
+
+let _fileExistsInDatabase = ( url ) => {
+  return Files.findOne( { "url": url, "userId": Meteor.userId() }, { fields: { "_id": 1 } } );
+};
+
+let _isNotAmazonUrl = ( url ) => {
+  return ( url.indexOf( 's3.amazonaws.com' ) < 0 );
+};
+
+let _validateUrl = ( url ) => {
+  if ( _fileExistsInDatabase( url ) ) {
+    return { valid: false, error: "Sorry, this file already exists!" };
+  }
+
+  if ( _isNotAmazonUrl( url ) ) {
+    return { valid: false, error: "Sorry, this isn't a valid URL!" };
+  }
+
+  return { valid: true };
+};
+
+let validate = ( url ) => {
+  let test = _validateUrl( url );
+
+  if ( !test.valid ) {
+    throw new Meteor.Error( "file-error", test.error );
+  }
+};
+
+
+
+
+
+
+
+Meteor.methods({
+  storeUrlInDatabase: function( url ) {
+    check( url, String );
+    Modules.both.checkUrlValidity( url );
+
+    try {
+      Files.insert({
+        url: url,
+        userId: Meteor.userId(),
+        added: new Date() 
+      });
+    } catch( exception ) {
+      return exception;
+    }
+  }
+});
+
+Modules      = {};
+Modules.client = {};
 
